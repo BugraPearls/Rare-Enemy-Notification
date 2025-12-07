a9654fb18b65fda0e04854434cff419360ff7450
BugraPearls <bugrapearls@gmail.com>
2025-12-07 20:24:21 +0300

    Add project files.

---
+++ ConfigOptions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader.Config;
+
+namespace RareEnemyNotification
+{
+    public class ConfigOptions : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [LabelKey("$Mods.RareEnemyNotification.TextColor")]
+        [TooltipKey("$Mods.RareEnemyNotification.TextColorTip")]
+        [DefaultValue(typeof(Color), "84, 252, 252, 255"), ColorNoAlpha]
+        public Color TextColor { get; set; }
+    }
+}
---
+++ RareEnemyNotification.cs
@@ -0,0 +1,62 @@
+using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace RareEnemyNotification
+{
+	enum MessageType
+    {
+        FoundRareEnemyToClients
+    }
+
+	public class RareEnemyNotification : Mod
+	{
+        public override void HandlePacket(BinaryReader reader, int whoAmI)
+        {
+            MessageType msgType = (MessageType)reader.ReadByte();
+            switch (msgType)
+            {
+                case MessageType.FoundRareEnemyToClients:
+                    EnemySpawn.NotifEffect(reader.ReadInt32());
+                    break;
+
+            }
+        }
+            }
+	public class EnemySpawn : GlobalNPC
+	{
+        public static void NotifEffect(int enemyType)
+        {
+            NPC npc = ContentSamples.NpcsByNetId[enemyType];
+            Main.NewText(Language.GetTextValue("Mods.RareEnemyNotification.ChatText").Replace("<name>",npc.TypeName));
+            CombatText.NewText(Main.LocalPlayer.getRect(), ModContent.GetInstance<ConfigOptions>().TextColor, Language.GetTextValue("Mods.RareEnemyNotification.PopupText").Replace("<name>", npc.TypeName), true);
+            SoundEngine.PlaySound(SoundID.Item35 with { PitchRange = (-0.9f, -0.2f) });
+        }
+        public override void OnSpawn(NPC npc, IEntitySource source)
+        {
+            if (Main.netMode == NetmodeID.SinglePlayer)
+            {
+                NotifEffect(npc.type);
+            }
+            else if (Main.netMode == NetmodeID.Server)
+            {
+                ModPacket packet = ModContent.GetInstance<RareEnemyNotification>().GetPacket();
+                packet.Write((byte)MessageType.FoundRareEnemyToClients);
+                packet.Write(npc.TypeName);
+                packet.Send();
+            }
+        }
+	}
+}
